7210adfc09090801b443a2bb7ba16db5fa3a19ed
Alexandre BRACH <alexandre.brach@gmail.fr>
2025-12-22 10:16:17 +0100

    fix: categorie table

--- backend-express/code/src/infra/typeorm/category/category.ts
+++ backend-express/code/src/infra/typeorm/category/category.ts
@@ -1,16 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity('category')
+@Entity('comptes_categorie')
 export class Category {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 200 })
   name!: string;
+
+  @Column({ type: 'varchar', length: 6, nullable: true })
+  color?: string;
 
   @Column({ type: 'int' })
   lft!: number;
 
   @Column({ type: 'int' })
   rght!: number;
+
+  @Column({ type: 'int' })
+  tree_id!: number;
+
+  @Column({ type: 'int' })
+  level!: number;
+
+  @Column({ type: 'int', nullable: true })
+  parent_id?: number;
 }
---
+++ claude.md
@@ -0,0 +1,11 @@
+# Architecture
+
+## Docker compose
+
+Se référer au fichier docker-compose pour :
+
+- connaitre les routes et les ports accessible de l'extérieur
+
+## Backend
+
+- l'application utilise yarn
