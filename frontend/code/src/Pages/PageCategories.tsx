384d71cf69e798a09bd46d67ac02d24a8be22d6e
Alexandre BRACH <alexandre.brach@gmail.fr>
2026-01-01 11:47:59 +0100

    refacto: format

--- frontend/code/src/Pages/PageCategories.tsx
+++ frontend/code/src/Pages/PageCategories.tsx
@@ -1,35 +1,31 @@
-
 import { FC, useContext, useEffect, useState } from 'react';
 import factory from '../services/Factory';
 import Layout from '../Layout';
 import CategoryTreeSelector from '../Components/CategoryTreeSelector';
 import { AppContext } from './AppContext';
-import "./PageCategories.scss"
+import './PageCategories.scss';
 import CategoryFormElement from '../Components/CategoryFormElement';
 import { ColorResult, HuePicker } from 'react-color';
 import { treeFindById } from '../exportable/Hierarchie/Tree';
 
 const Categories: FC = () => {
-
   const handleClickCreateButton = async () => {
-    let response = prompt("Nom pour la nouvelle catégorie :");
-    if ((null !== response) && ('' !== response.trim())) {
+    let response = prompt('Nom pour la nouvelle catégorie :');
+    if (null !== response && '' !== response.trim()) {
       await categoryService.create(response);
     }
-  }
+  };
 
   const refreshCategory = async () => {
-    dispatch({ type: "setAvailableCategories", data: { availableCategories: await categoryService.getTree() } })
-
-  }
+    dispatch({ type: 'setAvailableCategories', data: { availableCategories: await categoryService.getTree() } });
+  };
 
   const renameCategory = async (id: string) => {
-
     const oldName = treeFindById(state.availableCategories, id)!.name;
 
-    let name = prompt("Nouveau nom pour la catégorie :", oldName);
+    let name = prompt('Nouveau nom pour la catégorie :', oldName);
 
-    if ((null !== name) && ('' !== name.trim())) {
+    if (null !== name && '' !== name.trim()) {
       await categoryService.rename(id, name);
       await refreshCategory();
     }
@@ -41,21 +37,20 @@ const Categories: FC = () => {
       await categoryService.delete(id);
       await refreshCategory();
     }
-  }
+  };
 
   const moveCategoryIn = async (id: string, ancestorId: string) => {
     if (id === ancestorId) {
-      alert("Impossible de déplacer une catégorie dans elle-même.")
-
+      alert('Impossible de déplacer une catégorie dans elle-même.');
     } else {
       await categoryService.move(id, ancestorId);
       await refreshCategory();
     }
-  }
+  };
 
   const handleSelectCategory = (id: string | undefined) => {
     setSelected(id);
-  }
+  };
 
   const handleChangeSelectedColor = (value: ColorResult) => {
     setSelectedColor(value.hex);
@@ -64,30 +59,29 @@ const Categories: FC = () => {
   const handleValidateColor = async () => {
     await categoryService.updateColor(selected!, selectedColor!);
     await refreshCategory();
-  }
-
+  };
 
   const categoryService = factory.getCategoryService();
   const { state, dispatch } = useContext(AppContext);
   const [selected, setSelected] = useState<string | undefined>(undefined);
-  const [newAncestor, setNewAncestor] = useState('0')
+  const [newAncestor, setNewAncestor] = useState('0');
   const [selectedColor, setSelectedColor] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    const selectedColor: { color: string } | undefined = selected ? treeFindById(state.categoryColors, selected) : undefined;
+    const selectedColor: { color: string } | undefined = selected
+      ? treeFindById(state.categoryColors, selected)
+      : undefined;
     setSelectedColor(selectedColor ? selectedColor.color! : undefined);
-  }, [selected])
+  }, [selected]);
 
   const topIds = state.availableCategories.map((cat) => cat.id);
 
   const content = (
     <div id="page-categories">
       <div id="category-list" className="section no-background">
-        <input
-          type="button"
-          value="Créer une catégorie"
-          onClick={handleClickCreateButton}
-        /><br /><br />
+        <input type="button" value="Créer une catégorie" onClick={handleClickCreateButton} />
+        <br />
+        <br />
         <CategoryTreeSelector
           value={selected}
           colors={state.categoryColors}
@@ -96,47 +90,37 @@ const Categories: FC = () => {
           canSelectGroup={true}
         />
       </div>
-      {selected && (null !== selected) ?
-        (
-          <div className="section" id="category-edition">
-            <h1 style={{ color: selectedColor }}>{treeFindById(state.availableCategories, selected)!.name}</h1>
-            <div id="category-edition-control">
-              <div id="action">
-                <button onClick={(e) => renameCategory(selected)}>Renommer</button>
-                <button onClick={(e) => deleteCategory(selected)}>Supprimer</button>
-                {topIds.indexOf(selected) === -1 ||
-                  <>
-
-                    <div style={{ marginTop: '5px' }}>
-                      <HuePicker color={selectedColor!} onChangeComplete={handleChangeSelectedColor} />
-                    </div>
-                    <button onClick={handleValidateColor}>Modifier la couleur</button>
-                  </>
-                }
-              </div>
-              <div id="move-action">
-                Choisir un nouvel emplacement :
-                <CategoryFormElement
-                  value={newAncestor}
-                  onChange={(e) => setNewAncestor(e)}
-                  zeroLabel="--racine--"
-                />&nbsp;
-                <button onClick={
-                  (e) => moveCategoryIn(selected, newAncestor)
-                }>
-                  Déplacer
-                </button>
-              </div>
+      {selected && null !== selected ? (
+        <div className="section" id="category-edition">
+          <h1 style={{ color: selectedColor }}>{treeFindById(state.availableCategories, selected)!.name}</h1>
+          <div id="category-edition-control">
+            <div id="action">
+              <button onClick={(e) => renameCategory(selected)}>Renommer</button>
+              <button onClick={(e) => deleteCategory(selected)}>Supprimer</button>
+              {topIds.indexOf(selected) === -1 || (
+                <>
+                  <div style={{ marginTop: '5px' }}>
+                    <HuePicker color={selectedColor!} onChangeComplete={handleChangeSelectedColor} />
+                  </div>
+                  <button onClick={handleValidateColor}>Modifier la couleur</button>
+                </>
+              )}
+            </div>
+            <div id="move-action">
+              Choisir un nouvel emplacement :
+              <CategoryFormElement value={newAncestor} onChange={(e) => setNewAncestor(e)} zeroLabel="--racine--" />
+              &nbsp;
+              <button onClick={(e) => moveCategoryIn(selected, newAncestor)}>Déplacer</button>
             </div>
           </div>
-        )
-        :
-        <></>}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 
   return <Layout content={content} />;
-}
-
+};
 
 export default Categories;
